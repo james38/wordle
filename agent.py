4ddacea48d18246cc7b7aac3bfd9cb53a03823ca
James Taylor <james.taylor@gatech.edu>
2022-03-21 17:40:04 -0400

    implement lr warm-up and settling phases as well as timed increases in C parameter, as well as minor changes to other hyperparms including model architecture... additionally, remove some rude words from possible choices and a few possible answers mirroring NYT, clean up training, testing, and visualization code, and scale model inputs for forward pass

--- agent.py
+++ agent.py
@@ -7,25 +7,23 @@ import gym
 
 import torch
 import torch.nn as nn
-# import torch.nn.functional as F
 
 from wordle_rl import WordleEnv
 
 
 class Wordler(object):
     def __init__(self, device, verbose=False):
-        # self.env = gym.make(WordleEnv).env
         self.env = WordleEnv()
         self.env.seed(19)
         self.np_rng = np.random.default_rng(19)
 
         self.n_inputs = 183
 
-        self.alpha = 0.0003
+        self.alpha = 0.001
         self.epsilon = 0.999
         self.epsilon_decay = 0.99998
         self.min_epsilon = 0.02
-        self.gamma = 0.999
+        self.gamma = 0.9999
 
         self.device = device
         self.verbose = verbose
@@ -38,7 +36,9 @@ class Wordler(object):
         C=8,
         batch_size=64,
         max_experience=1000000,
-        clip_val=3,
+        clip_val=2,
+        warmup=2,
+        settle_at=0.01,
         lr_drop_at=0.9,
         lr_drop_rate=0.1,
     ):
@@ -50,6 +50,7 @@ class Wordler(object):
         for p in self.model.parameters():
             p.register_hook(lambda x: torch.clamp(x, -clip_val, clip_val))
         self.target_model = deepcopy(self.model)
+        C_init = C
         self.loss_fx = nn.MSELoss()
         self.optimizer = torch.optim.Adam(
             self.model.parameters(),
@@ -66,6 +67,17 @@ class Wordler(object):
                 for param_group in self.optimizer.param_groups:
                     param_group['lr'] *= lr_drop_rate
                 self.min_epsilon *= lr_drop_rate
+                C *= 4
+            elif self.n_episodes == int(settle_at * max_episodes):
+                for param_group in self.optimizer.param_groups:
+                    param_group['lr'] *= 0.3
+                C *= 4
+            if self.n_episodes < (warmup * C_init):
+                for param_group in self.optimizer.param_groups:
+                    param_group['lr'] = (
+                        self.alpha * (1 + self.n_episodes) / (warmup * C_init)
+                    )
+                C = max(2, int((1 + self.n_episodes) / warmup))
             self.state = self.env.reset()
             self.guessed = set()
             episode_R = 0
@@ -129,7 +141,7 @@ class Wordler(object):
                 )
 
                 y_preds = torch.gather(
-                    self.model(input),
+                    self.model(self.input_scaling(input)),
                     1,
                     torch.tensor(batch[:,-3].astype(int)).view(-1,1).to(device)
                 )
@@ -148,7 +160,7 @@ class Wordler(object):
                     self.state,
                     device=self.device
                 ).float()
-                state_logits = self.model(state_input)
+                state_logits = self.model(self.input_scaling(state_input))
                 y_preds_invalid = state_logits[invalid_action_inds]
 
                 y_preds = torch.cat((y_preds, y_preds_invalid.view(-1,1)))
@@ -174,6 +186,9 @@ class Wordler(object):
 
             if (self.n_episodes % C) == 0:
                 self.target_model = deepcopy(self.model)
+            if self.n_episodes == int(max_episodes / 2):
+                fsufx = dt.datetime.now().strftime("%Y%m%d.%H.%M.%S")
+                torch.save(self.model, f'./model_halftrain_{fsufx}')
             self.n_episodes += 1
             # q_val_converged = False
             # if (q_val_converged):
@@ -186,7 +201,7 @@ class Wordler(object):
 
         self.rewards = overall_rewards
 
-        return self.model
+        return self.model, fsufx
 
 
     def initialize_replay_memory(self, n):
@@ -206,7 +221,7 @@ class Wordler(object):
             action, _ = self.select_action(model, input)
         else:
             # action = np.random.randint(self.env.action_space.n)
-            valid_actions = self.env.info['valid_words'].keys()
+            valid_actions = list(self.env.info['valid_words'].keys())
             action = np.random.choice(valid_actions)
 
         return action
@@ -216,7 +231,7 @@ class Wordler(object):
         if type(input) is np.ndarray:
             input = torch.tensor(input.astype(np.float32)).to(self.device)
 
-        out = model(input)
+        out = model(self.input_scaling(input))
         valid_actions = self.env.info['valid_words'].keys()
         if exploit:
             invalid_actions = [
@@ -235,6 +250,10 @@ class Wordler(object):
         return action, best_action_value
 
 
+    def input_scaling(self, x):
+        return (x - 1) / 5
+
+
     def create_r_per_ep_fig(
         self,
         out_path='./training_reward_per_episode.png',
@@ -245,7 +264,9 @@ class Wordler(object):
 
         ax.set_xlabel('Episode Number')
         ax.set_ylabel('Reward')
-        ax.set_title(f'Agent training mean last {window} rewards{title_suffix}')
+        ax.set_title(
+            f'Agent training mean last {window} rewards\n{title_suffix}'
+        )
 
         from numpy.lib.stride_tricks import sliding_window_view
         window = min(window, len(self.rewards))
@@ -253,12 +274,12 @@ class Wordler(object):
             np.array(self.rewards), window_shape=window
         ).mean(axis=1)
         n_rolling = rolling.shape[0]
-
-        ax.plot(np.arange(n_rolling), rolling)
+        n_episodes = n_rolling + window
+        ax.plot(np.arange(window, n_episodes), rolling)
 
         plt.xticks(
             np.arange(
-                window, n_rolling + window, max(1, int(round(n_rolling / 10)))
+                window, n_episodes, max(1, int(round(n_episodes / 10)))
             )
         )
         plt.savefig(out_path)
@@ -273,11 +294,11 @@ class Model(nn.Module):
         # hidden_layer_3=256,
         hidden_layer_1=128,
         hidden_layer_2=64,
-        hidden_layer_3=16,
-        hidden_layer_4=16,
+        hidden_layer_3=32,
+        hidden_layer_4=32,
         hidden_layer_5=64,
         hidden_layer_6=128,
-        n_outputs=12972,
+        n_outputs=12947,
     ):
         super(Model, self).__init__()
         self.net = nn.Sequential(
@@ -342,19 +363,41 @@ def main(
     verbose=False,
 ):
     agent = Wordler(device, verbose)
-    agent.solve(
+    model, fsufx = agent.solve(
         model_loc=model_loc,
         max_episodes=max_episodes,
         C=C,
         batch_size=batch_size,
     )
 
-    agent.create_r_per_ep_fig()
+    agent.create_r_per_ep_fig(title_suffix=fsufx)
 
-    return None
+    return agent, fsufx
 
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    main(model_loc=None, max_episodes=50000, device=device, verbose=True)
-    # test_model(model_dir='./model_main_20220320', episodes=100)
+    mode = "train-test"
+    if "train" in mode and "test" in mode:
+        agent, fsufx = main(
+            model_loc=None,
+            max_episodes=50000,
+            device=device,
+            verbose=True,
+        )
+        history = test_model(
+            model_dir=f"./model_{fsufx}",
+            episodes=200,
+        )
+    elif "test" in mode:
+        history = test_model(
+            model_dir='./model_20220321.13.51.29',
+            episodes=100,
+        )
+    else:
+        agent, fsufx = main(
+            model_loc=None,
+            max_episodes=1000,
+            device=device,
+            verbose=True,
+        )
